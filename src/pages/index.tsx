d1a1a01fcc312626c1e708e51600394409ee01bd
jueinin <1014397160@qq.com>
2020-05-07 10:49:22 +0800

    remove grid bookItem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -118,32 +118,10 @@ const IndexPage = () => {
                 </nav>
                 <main data-name={'列表'} className="mt-4 p-2">
                     <div className="font-bold text-base">为您推荐</div>
-                    <div className="mt-4 grid grid-cols-2 gap-3 bg-gray-300">
+                    <div className="mt-4 ">
                         {data.map((list: BookBaseProperty[]) => {
                             return list.map((value,index) => {
-                                return <div key={value.bookId} className="bg-white rounded-lg grid grid-cols-1 gap-2"
-                                onClick={()=>{
-                                    push('/bookDetail?bookId=' + value.bookId);
-                                }}>
-                                    <img src={value.imgUrl} className="w-full"/>
-                                    <h2 className="mt-2 text-lg mx-2 truncate-2-lines " style={{
-                                        height: '3.2rem',
-                                        fontWeight: 520
-                                    }}>{value.title}</h2>
-                                    <h3 className="flex mt-1 mx-2 mb-1">
-                                        <span className="mr-auto text-lg font-bold">
-                                            <span className="" style={{marginRight: 2}}>￥</span>
-                                            {value.price.toString().split('.').map((value1,index) => {
-                                            if (index === 0) {
-                                                return <span key={index} className="text-lg">{value1}</span>
-                                            }
-                                            if (index === 1 && value1!=="0") {
-                                                return  <span key={index} className="text-sm">.{value1}</span>
-                                            }
-                                        })}</span>
-                                        <ShoppingCartOutlined className="ml-auto text-red-500"/>
-                                    </h3>
-                                </div>;
+                                return <BookItem {...value} onClick={()=>push('/bookDetail?bookId=' + value.bookId)}/>
                             });
                         })}
                     </div>
