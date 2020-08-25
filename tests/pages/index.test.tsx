1066df936333639c408790309db8e370172eb894
jueinin <b1014397160@gmail.com>
2020-08-24 18:45:56 -0700

    none

--- tests/pages/index.test.tsx
+++
@@ -1,8 +0,0 @@
-describe('test', function() {
-    it('should testss', function() {
-        function add(a: number, b: number):number {
-            return a+b
-        }
-        expect(add(2,3)).toEqual(5)
-    });
-});
